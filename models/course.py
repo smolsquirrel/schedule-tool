542c33f50b4ca70e1127de6f32038918bc47edd7
Jimmy <mimiouyang@hotmail.com>
2020-06-21 00:43:40 -0400

    Added course and daytime models

---
+++ models/course.py
@@ -0,0 +1,8 @@
+class Course:
+    def __init__(self,section,number,name,teacher,daytimes):
+        self.section = section
+        self.number = number
+        self.name = name
+        self.teacher = teacher
+        self.daytimes = daytimes
+
---
+++ models/daytime.py
@@ -0,0 +1,5 @@
+class Daytime:
+    def __init__(self,day,start,end):
+        self.day = day
+        self.start = start
+        self.end = end
