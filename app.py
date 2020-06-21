9b0ebaa6faad911ca3794257ab2b8d7e39c6a168
Jimmy <mimiouyang@hotmail.com>
2020-06-21 17:14:13 -0400

    Added Search, fixed initial load

--- app.py
+++ app.py
@@ -1,2 +1,141 @@
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from main import make_course_list
+import sys
+sys.path.append('./models')
+from course import Course
+from daytime import Daytime
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1111, 681)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setEnabled(True)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 1111, 661))
+        self.tabWidget.setMovable(False)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.tab)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(200, 10, 721, 80))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Open Sans")
+        font.setPointSize(26)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setScaledContents(False)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setWordWrap(False)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Open Sans")
+        font.setPointSize(26)
+        self.label_2.setFont(font)
+        self.label_2.setScaledContents(False)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.listWidget = QtWidgets.QListWidget(self.tab)
+        self.listWidget.setGeometry(QtCore.QRect(60, 150, 256, 192))
+        self.listWidget.setObjectName("listWidget")
+        item = QtWidgets.QListWidgetItem()
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(640, 290, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.search)
+        self.course_num_search = QtWidgets.QTextEdit(self.tab)
+        self.course_num_search.setGeometry(QtCore.QRect(410, 290, 221, 31))
+        self.course_num_search.setObjectName("course_num_search")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1111, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("Schedule Maker", "Schedule Maker"))
+        self.label.setText(_translate("MainWindow", "SECTION"))
+        self.label_2.setText(_translate("MainWindow", "COURSE NUMBER"))
+        __sortingEnabled = self.listWidget.isSortingEnabled()
+        self.listWidget.setSortingEnabled(False)
+        self.listWidget.setSortingEnabled(__sortingEnabled)
+        self.pushButton.setText(_translate("MainWindow", "Search"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+
+    def unique(self,course_list):
+        unique_course_num = []
+        for course in course_list:
+            if course.number not in unique_course_num:
+                unique_course_num.append(course.number)
+        self.unique_course_num = unique_course_num
+        return(unique_course_num)
+
+    def list_maker_setup(self, course_list):
+        for x in course_list:
+            item = QtWidgets.QListWidgetItem()
+            self.listWidget.addItem(item)
+
+    def list_maker_translated(self, course_list):
+        count = 0
+        _translate = QtCore.QCoreApplication.translate
+        for x in course_list:
+            item = self.listWidget.item(count)
+            item.setText(_translate("MainWindow", str(x)))
+            count += 1
+
+    def reset_list(self):
+        unique_all = self.unique_course_num
+        self.listWidget.clear()
+        self.list_maker_setup(unique_all)
+        self.list_maker_translated(unique_all)
+
+    def search(self):
+        keyword = self.course_num_search.toPlainText()
+        unique_all = self.unique_course_num
+        search_list = []
+        if keyword == '':
+            self.reset_list()
+        else:
+            for course in unique_all:
+                if keyword.upper() in course:
+                    search_list.append(course)
+            self.listWidget.clear()
+            self.list_maker_setup(search_list)
+            self.list_maker_translated(search_list)
+
+if __name__ == "__main__":
+    course_list = make_course_list("courses.csv")
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.list_maker_setup(ui.unique(course_list))
+    ui.list_maker_translated(ui.unique(course_list))
+    MainWindow.show()
+    sys.exit(app.exec_())
