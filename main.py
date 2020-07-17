3214f13d840969256e407be12f1022681b0e017e
Jimmy <mimiouyang@hotmail.com>
2020-07-17 13:35:11 -0400

    Reworked daytimes attribute, removed redundancies

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.append("./models")
-from course import Course
-from daytime import Daytime
+from course import Course, Week
 import re
+import pickle
+
 
 def readFile(filename):
-    file = open(filename, "r")
-    lines = file.readlines()
-    file.close()
-    return lines
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        return lines
 
 
 def time_conv(time):
@@ -30,35 +30,17 @@ def find_teacher(data):
         return teacher
 
 
-range_maker = lambda start, end: range(start, end)
-
-
 def make_course_list(filename):
-    new_line = lambda x: x != ""
     lines = readFile(filename)
     course_list = []
     course_dict = {}
-    empty_range = {
-        0: range(0, 0),
-        1: range(0, 0),
-        2: range(0, 0),
-        3: range(0, 0),
-        4: range(0, 0),
-        5: range(0, 0),
-    }
-    empty_list = {
-        "section": "",
-        "number": "",
-        "name": "",
-        "teacher": "",
-        "daytimes": empty_range,
-    }
     weekdays = {"M": 0, "T": 1, "W": 2, "H": 3, "F": 4, "S": 5}
-    temp = empty_list.copy()
+    temp = {"section": "", "number": "", "name": "", "teacher": "", "daytimes": ""}
     for line in lines:
         data = re.split(
             r',(?!\s|")', line.rstrip()
         )  # splits on comma, except for teacher name
+
         if data[0] != "":  # check if new course
             x = Course(
                 temp["section"],
@@ -70,16 +52,15 @@ def make_course_list(filename):
             key = "%s / %s" % (temp["number"], temp["section"])
             course_dict[key] = x
             course_list.append(x)
-            temp = empty_list.copy()  # resets dict
-            temp["daytimes"] = empty_range.copy()
+            temp["daytimes"] = {}
             temp["section"] = data[0]
             temp["number"] = data[1]
             temp["name"] = data[2]
             days = data[3]
             start, end = data[4].split("-")
             for day in days:
-                temp["daytimes"][weekdays[day]] = range_maker(
-                    time_conv(start), time_conv(end)
+                temp["daytimes"][day] = list(
+                    range(time_conv(start), time_conv(end), 30)
                 )
         else:
             teacher = find_teacher(data)
@@ -89,8 +70,8 @@ def make_course_list(filename):
                 days = data[3]
                 start, end = data[4].split("-")
                 for day in days:
-                    temp["daytimes"][weekdays[day]] = range_maker(
-                        time_conv(start), time_conv(end)
+                    temp["daytimes"][day] = list(
+                        range(time_conv(start), time_conv(end), 30)
                     )
 
     x = Course(
@@ -99,7 +80,7 @@ def make_course_list(filename):
     key = "%s / %s" % (temp["number"], temp["section"])
     course_dict[key] = x
     course_list.append(x)
-    course_list.pop(0)
+    course_list.pop(0)  # removes blank first course
 
     return [course_list, course_dict]
 
@@ -111,26 +92,9 @@ test_list = [
     ["203-NYA-05 / 21"],
     ["345-102-MQ / 5"],
     ["603-101-MQ / 1"],
-    ["GER-LAL / 1"],
+    ["GER-LAL / 1", "ITA-LAA / 3"],
 ]
 
 
-def get_times(tab, course_dict):
-    tab_list = []
-    weekdays = {"M": 0, "T": 1, "W": 2, "H": 3, "F": 4, "S": 5}
-    for elem in tab:
-        sch = {
-            0: range(0, 0),
-            1: range(0, 0),
-            2: range(0, 0),
-            3: range(0, 0),
-            4: range(0, 0),
-            5: range(0, 0),
-        }
-        obj = course_dict[elem].daytimes
-        for i in obj:
-            day, start, end = [i.day, i.start, i.end]
-            weekday = weekdays[day]
-            sch[weekday] = range(start, end)
-        tab_list.append((course_dict[elem], sch))
-    return tab_list
+def convList(submittedList):
+    return [iter(dic[elem] for elem in tab) for tab in submittedList]
